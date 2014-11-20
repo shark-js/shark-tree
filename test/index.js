d3adddb49f7e227bac64d264877d3124f749aaa3
Vadim Goncharov <vg@vadimgoncharov.ru>
2014-11-20 12:46:28 +0300

    Add tree src and tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,111 @@
 'use strict';
 
-describe('Initial test',function(){
+const expect    = require('chai').expect;
+const VError    = require('verror');
+const Tree      = require('../lib/tree');
+const SrcFiles  = require('../lib/src-files');
 
-	it('should output nothing',function(){
+describe('Initialization',function(){
+	it('should throw error if data is not {dst: src} like object', function() {
+		expect(function() {
+			new Tree([]);
+		}).to.throw();
 
+		expect(function() {
+			new Tree('hello world');
+		}).to.throw();
+	});
+
+	it('should throw error if data is empty object', function() {
+		expect(function() {
+			new Tree({});
+		}).to.throw('paths is empty object');
+	});
+
+	it('should not throw error if data is {dst: src} like object', function() {
+		expect(function() {
+			new Tree({
+				'dest/path': 'src/path'
+			});
+		}).not.to.throw();
+	});
+});
+
+describe('Parse paths', function() {
+	describe('strData', function() {
+		before(function() {
+			this.tree = new Tree({
+				'dest/path': 'src/path'
+			});
+		});
+
+		it('should have "dest/path" as property', function() {
+			expect(this.tree.hasDest('dest/path')).to.be.true;
+		});
+
+		it('should have "dest/path" as instanceof SharkSrcFiles', function() {
+			expect(this.tree.getSrcFilesByDest('dest/path')).to.be.an.instanceof(SrcFiles);
+		});
+
+		it('should output valid src', function() {
+			expect(this.tree.getSrcFilesByDest('dest/path').firstSrcFile().src()).equal('src/path');
+		});
+
+		it('should output valid srcFilesCount', function() {
+			expect(this.tree.getSrcFilesByDest('dest/path').srcFilesCount()).equal(1);
+		});
+	});
+
+	describe('arrData', function() {
+		before(function() {
+			this.tree = new Tree({
+				'dest/path': ['src/path/1', 'src/path/2']
+			});
+		});
+
+		it('should have "dest/path" as property', function() {
+			expect(this.tree.hasDest('dest/path')).to.be.true;
+		});
+
+		it('should have "dest/path" as instanceof SharkSrcFiles', function() {
+			expect(this.tree.getSrcFilesByDest('dest/path')).to.be.an.instanceof(SrcFiles);
+		});
+
+		it('should output valid src', function() {
+			expect(this.tree.getSrcFilesByDest('dest/path').srcFile(0).src()).equal('src/path/1');
+			expect(this.tree.getSrcFilesByDest('dest/path').srcFile(1).src()).equal('src/path/2');
+		});
+
+		it('should output valid srcFilesCount', function() {
+			expect(this.tree.getSrcFilesByDest('dest/path').srcFilesCount()).equal(2);
+		});
+	});
+
+	describe('objData', function() {
+		before(function() {
+			this.tree = new Tree({
+				'dest/path': {
+					files: ['src/path/1', 'src/path/2'],
+					options: {}
+				}
+			});
+		});
+
+		it('should have "dest/path" as property', function() {
+			expect(this.tree.hasDest('dest/path')).to.be.true;
+		});
+
+		it('should have "dest/path" as instanceof SharkSrcFiles', function() {
+			expect(this.tree.getSrcFilesByDest('dest/path')).to.be.an.instanceof(SrcFiles);
+		});
+
+		it('should output valid src', function() {
+			expect(this.tree.getSrcFilesByDest('dest/path').srcFile(0).src()).equal('src/path/1');
+			expect(this.tree.getSrcFilesByDest('dest/path').srcFile(1).src()).equal('src/path/2');
+		});
+
+		it('should output valid srcFilesCount', function() {
+			expect(this.tree.getSrcFilesByDest('dest/path').srcFilesCount()).equal(2);
+		});
 	});
 });
