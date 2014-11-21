35cb7c8bb11aa84c299f9eefce174399a5f71062
Vadim Goncharov <vg@vadimgoncharov.ru>
2014-11-21 12:15:41 +0300

    Add expand and make Tree yieldable

--- test/index.js
+++ test/index.js
@@ -16,234 +16,56 @@ describe('Initialization',function(){
 		});
 	});
 
-	it('should throw error if logger is not set', function() {
-		expect(function() {
-			Tree([]);
-		}).to.throw('logger argument is empty');
-
-		expect(function() {
-			Tree('hello world', {});
-		}).to.throw('logger is not instanceof SharkLogger');
-	});
-
-	it('should throw error if data is not {dst: src} like object', function() {
-		expect(function() {
-			Tree([], this.logger);
-		}.bind(this)).to.throw();
-
-		expect(function() {
-			Tree('hello world', this.logger);
-		}.bind(this)).to.throw();
-	});
-
-	it('should throw error if data is empty object', function() {
-		expect(function() {
-			Tree({}, this.logger);
-		}.bind(this)).to.throw('paths is empty object');
-	});
-
-	it('should not throw error if data is {dst: src} like object', function() {
-		expect(function() {
-			Tree({
-				'dest/path': 'src/path'
-			}, this.logger);
-		}.bind(this)).not.to.throw();
-	});
-});
-
-describe('Parse paths', function() {
-	before(function() {
-		this.logger = Logger({
-			name: 'SharkTreeLogger'
-		});
-	});
-
-	describe('strData', function() {
-		before(function() {
-			this.tree = Tree({
-				'dest/path': 'src/path'
-			}, this.logger);
-		});
-
-		it('should have "dest/path" as property', function() {
-			expect(this.tree.hasDest('dest/path')).to.be.true;
-		});
-
-		it('should have "dest/path" as instanceof SharkSrcFiles', function() {
-			expect(this.tree.getSrcCollectionByDest('dest/path')).to.be.an.instanceof(SrcCollection);
-		});
-
-		it('should output valid src', function() {
-			expect(this.tree.getSrcCollectionByDest('dest/path').getFirstFile().getSrc()).equal('src/path');
-		});
-
-		it('should output valid srcFilesCount', function() {
-			expect(this.tree.getSrcCollectionByDest('dest/path').getCount()).equal(1);
-		});
-	});
-
-	describe('arrData', function() {
-		before(function() {
-			this.tree = Tree({
-				'dest/path': ['src/path/1', 'src/path/2']
-			}, this.logger);
-		});
-
-		it('should have "dest/path" as property', function() {
-			expect(this.tree.hasDest('dest/path')).to.be.true;
-		});
-
-		it('should have "dest/path" as instanceof SharkSrcFiles', function() {
-			expect(this.tree.getSrcCollectionByDest('dest/path')).to.be.an.instanceof(SrcCollection);
-		});
-
-		it('should output valid src', function() {
-			expect(this.tree.getSrcCollectionByDest('dest/path').getFileByIndex(0).getSrc()).equal('src/path/1');
-			expect(this.tree.getSrcCollectionByDest('dest/path').getFileByIndex(1).getSrc()).equal('src/path/2');
-		});
-
-		it('should output valid srcFilesCount', function() {
-			expect(this.tree.getSrcCollectionByDest('dest/path').getCount()).equal(2);
-		});
-	});
-
-	describe('objData', function() {
-		before(function() {
-			this.tree = Tree({
-				'dest/path': {
-					files: ['src/path/1', 'src/path/2'],
-					options: {}
-				}
-			}, this.logger);
-		});
-
-		it('should have "dest/path" as property', function() {
-			expect(this.tree.hasDest('dest/path')).to.be.true;
-		});
-
-		it('should have "dest/path" as instanceof SharkSrcFiles', function() {
-			expect(this.tree.getSrcCollectionByDest('dest/path')).to.be.an.instanceof(SrcCollection);
-		});
-
-		it('should output valid src', function() {
-			expect(this.tree.getSrcCollectionByDest('dest/path').getFileByIndex(0).getSrc()).equal('src/path/1');
-			expect(this.tree.getSrcCollectionByDest('dest/path').getFileByIndex(1).getSrc()).equal('src/path/2');
-		});
-
-		it('should output valid srcFilesCount', function() {
-			expect(this.tree.getSrcCollectionByDest('dest/path').getCount()).equal(2);
-		});
-	});
-});
-
-describe('Fill content', function() {
-	before(function() {
-		this.logger = Logger({
-			name: 'SharkTreeLogger'
-		});
-
-		this.tree = Tree({
-			'dest/path': {
-				files: [
-					path.join(__dirname, './fixtures/1.txt'),
-					path.join(__dirname, './fixtures/2.txt'),
-					path.join(__dirname, './fixtures/3.txt'),
-				],
-				options: {}
-			}
-		}, this.logger);
-	});
-
-	it('should fill content from *.txt files', function *() {
-		var srcCollection = this.tree.getSrcCollectionByDest('dest/path');
-		yield srcCollection.fillContent();
-
-		expect(srcCollection.getCount()).equal(3);
-
-		expect(srcCollection.getFileByIndex(0).getContent()).equal('hello');
-		expect(srcCollection.getFileByIndex(1).getContent()).equal('world');
-		expect(srcCollection.getFileByIndex(2).getContent()).equal('lorem ipsum');
-	});
-
-	it('should fill content from *.txt files and transform to oneToOne', function *() {
-		this.tree = Tree({
-			'dest/path': {
-				files: [
-					path.join(__dirname, './fixtures/1.txt'),
-					path.join(__dirname, './fixtures/2.txt'),
-					path.join(__dirname, './fixtures/3.txt'),
-				],
-				options: {}
-			}
-		}, this.logger);
-
-
-		var srcCollection = this.tree.getSrcCollectionByDest('dest/path');
-		yield srcCollection.fillContent();
-		expect(srcCollection.getCount()).equal(3);
-
-		srcCollection.transformToOneToOne();
-		expect(srcCollection.getCount()).equal(1);
-
-		expect(srcCollection.getFirstFile().getContent()).equal('helloworldlorem ipsum');
-		expect(srcCollection.getFirstFile().getSrc()).to.be.null();
-	});
-});
-
-describe('write content', function() {
-	before(function *() {
-		this.logger = Logger({
-			name: 'SharkTreeLogger'
-		});
-
-		this.destPathA = path.join(__dirname, './fixtures/dest-a.txt');
-		this.destPathB = path.join(__dirname, './fixtures/dest-b.txt');
-
+	it('should parse {"dest/path": "src/path"}', function *() {
+		var dest = path.join(__dirname, './fixtures/dest-a.txt');
+		var src = path.join(__dirname, './fixtures/1.txt');
 		var files = {};
-		files[this.destPathA] = {
-			files: [
-				path.join(__dirname, './fixtures/1.txt'),
-				path.join(__dirname, './fixtures/2.txt'),
-				path.join(__dirname, './fixtures/3.txt'),
-			],
-			options: {}
-		};
-		files[this.destPathB] = {
-			files: [
-				path.join(__dirname, './fixtures/2.txt'),
-				path.join(__dirname, './fixtures/3.txt'),
-			],
-			options: {}
-		};
+		files[dest] = src;
 
-		this.tree = Tree(files, this.logger);
-
-		yield cofse.writeFile(this.destPathA, '');
-		yield cofse.writeFile(this.destPathB, '');
+		var tree = yield Tree(files, this.logger);
+		expect(tree.getSrcCollectionByDest(dest).getFileByIndex(0).getSrc()).equal(src);
 	});
 
-	it('should fill content from *.txt files and write it on the disk', function *() {
-		yield this.tree.fillContent();
+	it('should parse {"dest/path": ["src/path/1", "src/path/2"]}', function *() {
+		var dest = path.join(__dirname, './fixtures/dest-a.txt');
+		var src1 = path.join(__dirname, './fixtures/1.txt');
+		var src2 = path.join(__dirname, './fixtures/2.txt');
+		var files = {};
+		files[dest] = [src1, src2];
 
-		var collectionA = this.tree.getSrcCollectionByDest(this.destPathA);
-		var collectionB = this.tree.getSrcCollectionByDest(this.destPathB);
-
-		expect(collectionA.getCount()).equal(3);
-		expect(collectionB.getCount()).equal(2);
-
-		expect(collectionA.getFileByIndex(0).getContent()).equal('hello');
-		expect(collectionA.getFileByIndex(1).getContent()).equal('world');
-		expect(collectionA.getFileByIndex(2).getContent()).equal('lorem ipsum');
-
-		expect(collectionB.getFileByIndex(0).getContent()).equal('world');
-		expect(collectionB.getFileByIndex(1).getContent()).equal('lorem ipsum');
-
-		yield this.tree.writeContentToFiles();
-
-		var contentDestPathA = yield cofse.readFile(this.destPathA, {encoding: 'utf8'});
-		var contentDestPathB = yield cofse.readFile(this.destPathB, {encoding: 'utf8'});
-
-		expect(contentDestPathA).equal('helloworldlorem ipsum');
-		expect(contentDestPathB).equal('worldlorem ipsum');
+		var tree = yield Tree(files, this.logger);
+		expect(tree.getSrcCollectionByDest(dest).getFileByIndex(0).getSrc()).equal(src1);
+		expect(tree.getSrcCollectionByDest(dest).getFileByIndex(1).getSrc()).equal(src2);
 	});
+
+	it('should parse {"dest/path": {files: ["src/path/1", "src/path/2"], options: {test: "ipsum"}}}}', function *() {
+		var dest = path.join(__dirname, './fixtures/dest-a.txt');
+		var src1 = path.join(__dirname, './fixtures/1.txt');
+		var src2 = path.join(__dirname, './fixtures/2.txt');
+		var files = {};
+		files[dest] = {
+			files: [src1, src2],
+			options: {
+				test: 'ipsum'
+			}
+		};
+
+		var tree = yield Tree(files, this.logger);
+		expect(tree.getSrcCollectionByDest(dest).getFileByIndex(0).getSrc()).equal(src1);
+		expect(tree.getSrcCollectionByDest(dest).getFileByIndex(1).getSrc()).equal(src2);
+		expect(tree.getSrcCollectionByDest(dest).getOptions()).eql({test: 'ipsum'});
+	});
+
+	it('should exclude invalid path/2 {"dest/path": ["src/path/valid/1", "src/path/invalid/2"]}', function *() {
+		var dest = path.join(__dirname, './fixtures/dest-a.txt');
+		var src1 = path.join(__dirname, './fixtures/1.txt');
+		var src2 = path.join(__dirname, './fixtures/2222-invalid.txt');
+		var files = {};
+		files[dest] = [src1, src2];
+
+		var tree = yield Tree(files, this.logger);
+		expect(tree.getSrcCollectionByDest(dest).getFileByIndex(0).getSrc()).equal(src1);
+		expect(tree.getSrcCollectionByDest(dest).getCount()).equal(1);
+	});
+
 });
